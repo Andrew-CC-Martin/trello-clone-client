e9372ea1e5ce0fc67c5026557c601c1e79fa518b
Andrew-CC-Martin <amartin262@gmail.com>
2021-06-24 19:35:32 +1000

    fix form validation; add prod API URL

--- src/App.js
+++ src/App.js
@@ -21,15 +21,25 @@ function App() {
   const [cards, setCards] = useState([])
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [errorMessage, setErrorMessage] = useState("")
   const [inputsValid, setInputsValid] = useState(true)
 
+  // call the API on page load to get the cards
   useEffect(() => {
     api.get("/cards")
       .then(({ data }) => setCards(data))
       .catch(({ message }) => setErrorMessage(message))
+      .finally(setLoading(false))
   }, [])
+
+  // whenever title and description are updated, run them through the validators
+  useEffect(() => {
+    // run the input through the validator function
+    const isValid = validateInput(title) && validateInput(description)
+
+    setInputsValid(isValid)
+  }, [title, description])
 
   const addCard = async (event) => {
     event.preventDefault()
@@ -66,21 +76,15 @@ function App() {
     }
   }
 
-  const handleTextChange = (event, setter) => {
-    setter(event.target.value)
-
-    // run the input through the validator function
-    const isValid = validateInput(title) && validateInput(description)
-    setInputsValid(isValid)
-  }
-
-  if (cards.length < 1) {
-    return <CircularProgress />
-  }
-
   return (
     <>
       <CssBaseline />
+      {loading && <CircularProgress />}
+      <div>
+      <form>
+        <input type="hidden" defaultValue={process.env.REACT_APP_NOT_SECRET_CODE} />
+      </form>
+    </div>
       <Box>
         <Typography variant="h4" component="h1">trello clone</Typography>
         {cards.map(({ title, description, id }) => (
@@ -96,14 +100,13 @@ function App() {
 
         <form onSubmit={addCard}>
           <FormControl error={!inputsValid}>
-            <TextField error={!inputsValid} onChange={(e) => handleTextChange(e, setTitle)} value={title} id="title" label="Title" />
-            <TextField error={!inputsValid} onChange={(e) => handleTextChange(e, setDescription)} value={description} id="description" label="Description" />
+            <TextField error={!inputsValid} onChange={(e) => setTitle(e.target.value)} value={title} id="title" label="Title" />
+            <TextField error={!inputsValid} onChange={(e) => setDescription(e.target.value)} value={description} id="description" label="Description" />
             <Button type="submit" disabled={!inputsValid}>Add card</Button>
             <FormHelperText id="my-helper-text">Please don't use any naughty language</FormHelperText>
           </FormControl>
         </form>
 
-        {loading && <CircularProgress />}
         {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
       </Box>
     </>
--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,12 @@
 import axios from "axios"
 
-const apiUrl = process.env.REACT_APP_API_URL
+// const apiUrl = process.env.REACT_APP_API_URL
+let apiUrl
+if (process.env.NODE_ENV === "production") {
+  apiUrl = "https://stark-mountain-99923.herokuapp.com/"
+} else {
+  apiUrl = process.env.REACT_APP_API_URL
+}
 
 export const api = axios.create({
   baseURL: apiUrl
